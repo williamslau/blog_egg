a4fd55a7e6cf678c8abaec96aeeb78798a335ecf
DESKTOP-RFOHM18\williamlau <liuyz@mydubang.com>
2018-05-15 22:00:25 +0800

    blog2

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,5 +1,7 @@
 const { Controller } = require('egg');
 class HomeController extends Controller {
+    // ctx上下文对象 它里面有很多属性和方法 request response
+    // ctx.body=ctx.response.body
     async index() {
         this.ctx.body = 'hello world';
     }
